9751f04e1561bb3eb69cbebc21218bdcd5d354d4
zagpro1408 <zagorskybobuyc@gmail.com>
2017-03-31 19:27:25 +0300

    Set up saving data from order form into the db

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,7 +1,21 @@
 class MainPageController < ApplicationController
+
+
   def index
   end
 
   def create
+    @order = Order.new order_params
+
+    if @order.save
+      redirect_to root_path
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:company, :name, :email,  :number, :text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resource :main_page, only: [:index, :create]
+  post '/' => 'main_page#create'
 
   root to: 'main_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
